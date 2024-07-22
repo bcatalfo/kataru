822c0ccb13791a1bbcfbf9bdd7477cee42b916f5
Benjamin Catalfo <bcatalfo@gmail.com>
2024-07-22 17:36:38 -0400

    Now actually generates stories and lets you pick your language!

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kataru',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red[700]!),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Kataru'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -60,71 +37,191 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _story = 'Swipe down to generate a new story.';
+  String _nativeLanguage = 'English';
+  String _targetLanguage = 'Japanese';
 
-  void _incrementCounter() {
+  final List<String> _languages = [
+    'English',
+    'Japanese',
+    'Spanish',
+    'French',
+    'German',
+    'Italian',
+    'Mandarin (Simplified)',
+    'Mandarin (Traditional)',
+    'Korean',
+    'Russian',
+    'Portuguese',
+    'Arabic',
+    'Hindi',
+    'Bengali',
+    'Punjabi',
+    'Javanese',
+    'Vietnamese',
+    'Turkish',
+    'Thai',
+    'Polish',
+    'Dutch',
+    'Greek',
+    'Czech',
+    'Swedish',
+    'Hungarian',
+    'Finnish',
+    'Danish',
+    'Norwegian',
+    'Hebrew',
+    'Malay',
+    'Indonesian',
+    'Filipino',
+    'Swahili',
+    'Zulu',
+    'Hausa',
+    'Yoruba',
+    'Amharic',
+    'Nepali',
+    'Sinhala',
+    'Telugu',
+    'Tamil',
+    'Marathi',
+    'Gujarati',
+    'Kannada',
+    'Malayalam',
+    'Urdu',
+    'Persian',
+    'Pashto',
+    'Burmese',
+    'Khmer',
+    'Lao',
+    'Mongolian',
+    'Uzbek',
+    'Kazakh',
+    'Tajik',
+    'Turkmen',
+    'Kurdish',
+    'Serbian',
+    'Croatian',
+    'Bosnian',
+    'Slovak',
+    'Slovenian',
+    'Bulgarian',
+    'Romanian',
+    'Ukrainian',
+    'Belarusian',
+    'Lithuanian',
+    'Latvian',
+    'Estonian',
+    'Icelandic',
+    'Maltese',
+    'Luxembourgish',
+    'Albanian',
+    'Georgian',
+    'Armenian',
+    'Azerbaijani'
+  ];
+
+  Future<void> _generateNewStory() async {
+    final model =
+        FirebaseVertexAI.instance.generativeModel(model: 'gemini-1.5-flash');
+    final prompt = [
+      Content.text(
+          'Write a simple story in $_targetLanguage and translate it into $_nativeLanguage.')
+    ];
+    final response = await model.generateContent(prompt);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _story = response.text ?? 'No story generated';
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  void initState() {
+    super.initState();
+    _generateNewStory();
+  }
+
+  void _showSettingsDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Settings'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              DropdownButton<String>(
+                value: _nativeLanguage,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _nativeLanguage = newValue!;
+                  });
+                },
+                items: _languages.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              DropdownButton<String>(
+                value: _targetLanguage,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _targetLanguage = newValue!;
+                  });
+                },
+                items: _languages.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _generateNewStory();
+              },
             ),
           ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: _showSettingsDialog,
+          ),
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: _generateNewStory,
+        child: Center(
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  _story,
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
